e7fb55fcd1ad1324180d159b3f818c20f0e7fdc0
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-06 21:32:06 +0000

    build based on 2a154ad

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#FluorescenceExclusion.jl-1","page":"Home","title":"FluorescenceExclusion.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"A metapackage for analyzing fluorescence exclusion microscopy (FxM) images in Julia. Very much a WIP.","category":"page"}]
+[{"location":"#FluorescenceExclusion.jl","page":"Home","title":"FluorescenceExclusion.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A metapackage for analyzing fluorescence exclusion microscopy (FxM) images in Julia. Very much a WIP.","category":"page"}]
 }
