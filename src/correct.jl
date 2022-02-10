4e35503b8222970890a5255f1bc71a39acad2bcf
Tamas Nagy <tamas@tamasnagy.com>
2022-02-09 22:37:01 -0800

    allow passing an additional mask

--- src/correct.jl
+++ src/correct.jl
@@ -1,22 +1,30 @@
 using StatsBase
 
-function correct!(data::AbstractArray{<: Colorant, 3}, dn_img::AbstractArray{<: Colorant, 3})
+function correct!(data::AbstractArray{<: Colorant, 3}, dn_img::AbstractArray{<: Colorant, 3}; mask = nothing)
     labels = Array{Int}(undef, size(dn_img))
+    (mask !== nothing) && (@assert size(data) == size(mask))
 
     @showprogress for t in 1:size(dn_img, 3)
         slice = view(data, :, :, t)
         dn_slice = view(dn_img, :, :, t)
 
-        labels[:, :, t] = correct!(slice, dn_slice)
+        if mask !== nothing
+            labels[:, :, t] = correct!(slice, dn_slice; mask=mask[:, :, t])
+        else
+            labels[:, :, t] = correct!(slice, dn_slice)
+        end
     end
     labels
 end
 
-function correct!(data::AbstractArray{<: Colorant, 2}, dn_img::AbstractArray{<: Colorant, 2})
+function correct!(data::AbstractArray{<: Colorant, 2}, dn_img::AbstractArray{<: Colorant, 2}; mask=zeros(Bool, size(data)...)) 
     pillar_mask, pillar_centers = identify(Pillars(), dn_img)
-    cell_mask = identify(Cells(), dn_img, pillar_mask)
+    cell_mask = identify(Cells(), dn_img, pillar_mask) .| mask
 
-    flatfield = compute_flatfield(data, cell_mask .| pillar_mask, len=20);
+    pillar_medians = get_medians(data, pillar_centers)
+    bkg = mean(values(pillar_medians))
+
+    flatfield = compute_flatfield(data, cell_mask .| pillar_mask, len=20) .- bkg;
 
     labeled = label_components(cell_mask)
 
